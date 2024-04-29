f883412709d67bcb12f1c3a29516f21696fb16d1
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-29 13:14:36 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+
+import React,{useEffect,useState} from "react";
+
+function App(){
+    
+    const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+        setDogImage(data.message)
+        setLoading(false)
+    })
+},[]);
+
+return(
+    <div>
+       {loading ?( <p>Loading...</p>) :(
+        <img src={dogImage} alt="A Random Dog"/>
+       )} 
+    </div>
+)
+}
+
+export default App
